4952e1ca62292bb2d47009e4f96d8d7c8816b725
unknown <v-cmoody@.redmond.corp.microsoft.com>
2012-05-25 11:44:49 -0500

    initial commit

---
+++ assets/js/console.js
@@ -0,0 +1,12 @@
+(function($){
+	
+	var commands = [
+		{ name: "hud_show 0"},
+		{ name: "hud_show 1"}
+	];
+	
+})(JQuery);
+
+
+// pressing up shows history and typing autocompletes from command list
+// Store history in array and check the length to make sure its not more that X
