c00de4065a23d93ae61649f7f2dc46a6b50a1e53
unknown <chase@techhiker.com>
2012-05-25 15:02:44 -0500

    Renamed some files Removed the backbone and underscore libs will add back into other version
    Created history using up key in input box

--- assets/js/console.js
+++ assets/js/console.js
@@ -1,12 +1,55 @@
-(function($){
+(function(){
 	
-	var commands = [
-		{ name: "hud_show 0"},
-		{ name: "hud_show 1"}
+	/* Global Variables */
+	cmdHistory = [];
+	count = cmdHistory.length-1;
+	
+	/* List of available commands */
+	var cmdAvailable = [
+		{ name: "apples" },
+		{ name: "hud_show 0" },
+		{ name: "hud_show 1" },
+		{ name: "screenshot" }
 	];
 	
-})(JQuery);
+	/* On click of send button take whats in input and send it */
+	$('.send').click(function(){
+		var cmd = $('.cmd').val();
+		
+		cmdHistory.push(cmd);
+		
+		/* To keep the array from getting too large */
+		//if(cmdHistory.lenght > 25) cmdHistory.splice(0,1);
+		
+		$('#console ul').append('<li>&gt; ' + cmd + '</li>');
+		
+		/* Send command to node.js server on websocket */
+		// socket.emit('cmd',cmd);
+		
+		// Resets input field & history count
+		$('.cmd').val('');
+		count = cmdHistory.length-1;
+	});
+	
+	/* Arrow key up goes through input history */
+	$('.cmd').keyup(function(e) {
+	  if (e.keyCode == 38) {
+	  	$('.cmd').val(cmdHistory[count]);
+	  	count = count-1;
+	  }
+	});
+	
+	// $('#console ul').append('<li>Console: Response');
+	/*
+		socket.on()
+	*/
+	
+})();
 
-
-// pressing up shows history and typing autocompletes from command list
-// Store history in array and check the length to make sure its not more that X
+/* 
+ * 
+ * Whats needed
+ * Command list when typing
+ * Command History whats been typed
+ * 
+ */
